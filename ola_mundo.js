24f4569a71b3a3f0b1fb92dcbab96ea11d46d313
fillipe22santos <yungzitoty@gmail.com>
2026-03-02 21:03:43 -0300

    add variaveisOperadores e ola mundo

---
+++ ola_mundo.js
@@ -0,0 +1,8 @@
+let msg = "olá, mundo! ";
+console.log (msg);
+
+let A = 2;
+let B = 3;
+let C = A + B; // +, -, *, / = operadores númericos basicos//
+
+console.log (`A soma entre ${A} e ${B} é igual a ${C}`);
