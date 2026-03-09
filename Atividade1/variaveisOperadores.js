816948dedb014c8691a7e4b18e0c7438c84491b9
fillipe22santos <yungzitoty@gmail.com>
2026-03-09 13:49:39 -0300

    add atividade1

---
+++ Atividade1/variaveisOperadores.js
@@ -0,0 +1,18 @@
+let num_A = 10;
+let num_B = 5;
+
+// Soma
+let soma = num_A + num_B;
+console.log(`Resultado da soma entre ${num_A} e ${num_B} é ${soma}`);
+
+// Subtração
+let subtracao = num_A - num_B;
+console.log(`Resultado da subtração entre ${num_A} e ${num_B} é ${subtracao}`);
+
+// Multiplicação
+let multiplicacao = num_A * num_B;
+console.log(`Resultado da multiplicação entre ${num_A} e ${num_B} é ${multiplicacao}`);
+
+// Divisão
+let divisao = num_A / num_B;
+console.log(`Resultado da divisão entre ${num_A} e ${num_B} é ${divisao}`);
