b54a066bf7ac8d4b43b9cdf5d45230be89333851
fillipe22santos <yungzitoty@gmail.com>
2026-04-06 20:06:01 -0300

    add index

--- Atividade3/index.js
+++ Atividade3/index.js
@@ -1,1 +1,49 @@
-dsfflgkldfklnsdf
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+let estoque = [];
+
+app.get('/adicionar/:id/:nome/:qtd', (req, res) => {
+    const { id, nome, qtd } = req.params;
+
+    const produto = {
+        id: id,
+        nome: nome,
+        qtd: parseInt(qtd)
+    };
+
+    estoque.push(produto);
+
+    res.send(`Produto ${nome} adicionado!`);
+});
+
+app.get('/listar', (req, res) => {
+    res.json(estoque);
+});
+
+app.get('/remover/:id', (req, res) => {
+    const { id } = req.params;
+
+    estoque = estoque.filter(p => p.id !== id);
+
+    res.send(`Produto removido!`);
+});
+
+app.get('/editar/:id/:qtd', (req, res) => {
+    const { id, qtd } = req.params;
+
+    const produto = estoque.find(p => p.id === id);
+
+    if (produto) {
+        produto.qtd = parseInt(qtd);
+        res.send('Quantidade atualizada!');
+    } else {
+        res.send('Produto não encontrado!');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
